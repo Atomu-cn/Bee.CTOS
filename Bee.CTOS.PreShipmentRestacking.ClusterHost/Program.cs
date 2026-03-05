c08465891f6ed352077eeccfa9603534d9eb52c0
phenixiii <phenixiii@126.com>
2026-03-05 09:33:29 +0800

    搭建解决方案结构

---
+++ Bee.CTOS.PreShipmentRestacking.ClusterHost/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Bee.CTOS.PreShipmentRestacking.ClusterHost
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
